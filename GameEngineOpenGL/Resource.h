fef9dd495069839b5abc01e0236b648af58f055c
donutAnees <pvmohammedanees2003@gmail.com>
2025-05-11 20:58:09 +0530

    Create object from STL File

--- GameEngineOpenGL/Resource.h
+++ GameEngineOpenGL/Resource.h
@@ -1,48 +1,36 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by GameEngineOpenGL.rc
-
-// App and Icon IDs
-#define IDS_APP_TITLE            100
-#define IDI_GAMEENGINEOPENGL     101
-#define IDI_SMALL                102
-#define IDC_GAMEENGINEOPENGL     103
-#define IDC_MYICON               104
-
-// Dialogs
-#define IDD_GAMEENGINEOPENGL_DIALOG  200
-#define IDD_ABOUTBOX                 210
-#define IDD_OBJECT_DIALOG            220
-
-// Menu Commands
-#define IDM_ABOUT                300
-#define IDM_EXIT                 301
-#define IDM_VIEW_ORTHO           310
-#define IDM_VIEW_PERSP           311
-#define IDM_OBJECT               320
-
-// Controls inside dialogs 
-#define IDC_OBJECT_TYPE          1000
-#define IDC_POSITION_X           1001
-#define IDC_POSITION_Y           1002
-#define IDC_POSITION_Z           1003
-#define IDC_SIZE                 1004
-
-// Static placeholder
-#ifndef IDC_STATIC
-#define IDC_STATIC               -1
-#endif
-
+//
+#define IDS_APP_TITLE                   100
+#define IDI_GAMEENGINEOPENGL            101
+#define IDI_SMALL                       102
+#define IDC_GAMEENGINEOPENGL            103
+#define IDC_MYICON                      104
+#define IDD_GAMEENGINEOPENGL_DIALOG     200
+#define IDD_ABOUTBOX                    210
+#define IDD_OBJECT_DIALOG               220
+#define IDM_ABOUT                       300
+#define IDM_EXIT                        301
+#define IDM_VIEW_ORTHO                  310
+#define IDM_VIEW_PERSP                  311
+#define IDM_OBJECT                      320
+#define IDC_OBJECT_TYPE                 1000
+#define IDC_POSITION_X                  1001
+#define IDC_POSITION_Y                  1002
+#define IDC_POSITION_Z                  1003
+#define IDC_SIZE                        1004
+#define ID_CREATE_FILE                  32771
+#define IDC_STATIC                      -1
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
